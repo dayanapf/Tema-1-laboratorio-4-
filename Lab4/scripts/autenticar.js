32645a9b1c1d1d26ea666e452f556f78387a88ac
dayanapf <84345203+dayanapf@users.noreply.github.com>
2022-02-17 03:32:51 +0100

    laboratorio

---
+++ Lab4/scripts/autenticar.js
@@ -0,0 +1,75 @@
+// Ejercicio 1
+alert("Primer programa en JavaScript");
+
+/* Ejercicio 2
+Profe quise agregar cada una de las situaciones posibles en las cuales el formulario no fuera válido
+*/
+
+window.onsubmit = function autenticar(){
+    
+    var usuario = document.formulario.nombre;
+    var pass = document.formulario.pass;
+    var correo = document.formulario.correo; 
+
+    var usurio_vacio = esta_vacio(usuario);
+    var pass_vacio = esta_vacio(pass);
+    var correo_vacio = esta_vacio(correo);
+    
+    if(usurio_vacio && pass_vacio  && correo_vacio)
+    {
+        usuario.style.borderColor = 'red';
+        pass.style.borderColor = 'red';
+        correo.style.borderColor = 'red';
+        alert("Los campos estan vacios");
+    }
+    else if((usurio_vacio && pass_vacio) || (usurio_vacio && correo_vacio) || (pass_vacio && correo_vacio) )
+    {
+        var alert_vacios= [];
+        var i = 0;
+        if(usurio_vacio){        
+            alert_vacios [i] = "usuario ";
+            usuario.style.borderColor = 'red';
+            i++;
+        }
+        if(pass_vacio){        
+            alert_vacios [i] = "password ";  
+            pass.style.borderColor = 'red';
+            i++;      
+        }
+        if(correo_vacio){             
+            alert_vacios [i] = "correo ";
+            correo.style.borderColor = 'red';
+            i++;
+        }
+        alert("Los campos " + alert_vacios + " no pueden estar vacios");
+    }
+    else if(usurio_vacio || pass_vacio || correo_vacio){
+        if(usurio_vacio){   
+            usuario.style.borderColor = 'red';
+            alert("El campo usuario no puede estar vacio");
+        }
+        else if(pass_vacio){        
+            pass.style.borderColor = 'red';
+            alert("El campo password no puede estar vacio");      
+        }
+        else if(correo_vacio){             
+            correo.style.borderColor = 'red';
+            alert("El campo correo no puede estar vacio");
+        }        
+        
+    }
+    else {
+        alert("Formulario válido");
+    }
+}
+
+ function esta_vacio(valor){
+    var vacio = false;
+    var exp_no_espacio = /^\s+$/;
+    if (!valor.value || valor.value.length == 0 || exp_no_espacio.test(valor.value) )
+    {
+        return true;
+    }
+    return vacio;
+}
+// Concluye Ejercicio 2
---
+++ Lab4/scripts/datos.js
@@ -0,0 +1,94 @@
+
+window.onsubmit = function salvarDatos(){
+    
+    var nombre = document.formulario.nombre;
+    var apellidos = document.formulario.apellidos;
+    var genero = document.formulario.genero;  
+    var foto = document.formulario.foto;  
+
+
+//variables auxiliares
+    var campos_vacios = ["Error\n"];
+    var nombre_vacio = esta_vacio(nombre);
+    var apellidos_vacio = esta_vacio(apellidos);
+
+
+//nombre
+    if(nombre_vacio) 
+    {
+        if(nombre_vacio) campos_vacios.push("Falta el Nombre\n");  
+        
+    } else {
+        if(!empesar_con_mayuscula(nombre.value))
+            campos_vacios.push("El nombre debe empesar con Mayuscula y con solo un especio de por medio\n");
+    }
+
+//apellido
+    if(apellidos_vacio) 
+    { 
+        if(apellidos_vacio) campos_vacios.push("Faltan los Apellidos\n");
+    } else {
+        if(!empesar_con_mayuscula(apellidos.value))
+            campos_vacios.push("El apellido debe empesar con Mayuscula y con solo un especio de por medio\n");
+    }
+
+
+//genero
+    if(!genero.value){
+        campos_vacios.push("Se debe seleccionar un genero\n");
+    }
+// Foto
+    if(foto.value)
+    {
+        var exp_jpg_bmp=/(.jpg|.bmp)$/; 
+     
+        if(!exp_jpg_bmp.test(foto.value)){
+            campos_vacios.push("La foto debe ser extención '.jpg' o '.bmp'\n");
+        }
+
+    }
+
+    if(campos_vacios.length > 1)
+    {
+        alert(campos_vacios);
+    }
+    else{
+        alert("Formulario válido");
+    }     
+    
+}
+
+function habilitarInput() {
+    var otro = document.formulario.otrochb;
+    if(otro.checked){
+        document.getElementById("activar").disabled = false; 
+    }else
+    document.getElementById("activar").disabled = true;
+}
+
+
+//metodos auxiliares
+
+//verificar que un campo de texto este realmente vacio
+function esta_vacio(valor){
+    var vacio = false;
+    var exp_no_espacio = /^\s+$/;
+    if (!valor.value || valor.value.length == 0 || exp_no_espacio.test(valor.value) )
+    {
+        return true;
+    }
+    return vacio;
+}
+
+//comprobar que el o los nombre empiesan con mayuscula y tienen solo letras
+function empesar_con_mayuscula(cadena){
+    var exp = /^[A-Z][a-z_]/;  
+    var todas_empiezan_con_mayuscula = true;    
+    var dividido = cadena.split(/\s/);
+    for(i = 0, len = dividido.length; i < len; i++){
+        if(!exp.test(dividido[i])){
+            todas_empiezan_con_mayuscula = false;
+        }
+    }
+    return todas_empiezan_con_mayuscula;
+}
